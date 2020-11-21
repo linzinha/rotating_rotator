e15d183dabf057f3d23c7ae5ad3b78bdab5e8dae
linzinha <lindsey.m@posteo.net>
2020-11-21 22:29:52 +0000

    added skeleton to main file

---
+++ rotating_rotator.py
@@ -0,0 +1,12 @@
+#############################
+# rotating_rotator
+#############################
+
+# First define the cipher
+# Second Ask if they have keys??
+# Third Set Keys
+# Fourth open text editor
+# Fifth decode position key
+# Sixth write text file and append to position key
+# Seventh encode position key
+# Eighth append to another document (optional)
